cf7808ae9527b25493cf05a301971065d667765d
Jana Meyer <janmeyer@csumb.edu>
2019-10-29 10:56:01 -0700

    Project 4 adding all pages

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ app.use(express.static("public"));
 
 //routes
 app.get("/", function (req, res) {
-    res.send("Hallo");
-    //res.render("index.html");
+    res.render("index.html");
+});
+app.get("/types", function (req, res) {
+    res.render("types.html");
+});
+app.get("/challenges", function (req, res) {
+    res.render("challenges.html");
+});
+app.get("/testing", function (req, res) {
+    res.render("testing.html");
 });
 
 
